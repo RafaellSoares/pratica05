cd1f2b80b12ef6aa507c0bbdf782f7d8310d704a
RafaellSoares <rafaelsoares199911@gmail.com>
2022-05-09 17:16:26 -0300

    alterado

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
-const lista = document.querySelector("ol");
+const lista = document.querySelector("ol li");
 
-const itensLista1 = lista.querySelectorAll("li");
+// const itensLista1 = lista.querySelectorAll("li");
 
-for(let i = 0; i< itensLista1.length; i++){
-    if((i+1) % 2 === 0){
-        itensLista1[i].style.fontWeight = 900;
-    }else{
-        itensLista1[i].style.color = "red";
-    }
-}
+// for(let i = 0; i< itensLista1.length; i++){
+//     if((i+1) % 2 === 0){
+//         itensLista1[i].style.fontWeight = 900;
+//     }else{
+//         itensLista1[i].style.color = "red";
+//     }
+// }
 
 // V2
 // const itensLista2 = document.querySelectorAll("ol li");
 // V3
 // const itensLista2 = document.getElementsByTagName("li");
 // V4
-// itensLista1.forEach((el, idx) =>{
-//     if((idx+1) % 2 === 0){
-//         el.style.fontWeight = 900;
-//     }
-// })
+itensLista1.forEach((el, idx) =>{
+    if((idx+1) % 2 === 0)
+        el.style.fontWeight = 900;
+})
 
